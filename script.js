3fbe53783a9ce0ea8364fa4651ffbb58e9995174
Sridharan-16 <kit27.am53@gmail.com>
2025-07-07 06:48:22 +0530

    Separate File

---
+++ script.js
@@ -0,0 +1,23 @@
+let array = [];
+        console.log("hello world"); 
+        document.addEventListener("keydown", function(event) {
+            if (event.key === "Enter") {
+                let input = document.getElementById("todoInput");
+                array.push(input.value);
+                if(input.value.length > 0 ) {
+                    let list = document.getElementById("list");
+                    list.style.display = "block";
+                    list.innerHTML += '<li>'+input.value+'  '+'<button onClick="remove(event)">'+'X'+'</button></li>';
+                    input.value = "";
+                }
+            }
+        });
+        function remove(event) {
+            if (event.target.tagName === "BUTTON") {
+                let li = event.target.parentNode;
+                li.parentNode.removeChild(li);
+                if (document.getElementById("list").children.length === 0) {
+                    document.getElementById("list").style.display = "none";
+                }
+            }
+        }
